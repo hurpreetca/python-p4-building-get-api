36008743e43abd94ebd33808c7385d2eaebd4b77
Harpreet Singh <hurpreet@icloud.com>
2023-09-26 00:08:01 -0400

    Lab Completed

--- server/app.py
+++ server/app.py
@@ -7,17 +7,36 @@ from flask_migrate import Migrate
 from models import db, User, Review, Game
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return "Index for Game/Review/User API"
 
-if __name__ == '__main__':
+
+@app.route("/games")
+def games():
+    games = []
+    for game in Game.query.all():
+        game_dict = {
+            "title": game.title,
+            "genre": game.genre,
+            "platform": game.platform,
+            "price": game.price,
+        }
+        games.append(game_dict)
+
+    response = make_response(jsonify(games), 200)
+
+    return response
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
